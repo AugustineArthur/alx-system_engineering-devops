51fbe8900642d03e18b728d7f81c21a45f7cbf1c
Eric Augustine Arthur <augustinearthur07@gmail.com>
2023-03-02 01:05:06 -0800

    Task updates

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Use strace to find out why Apache is returning a 500 error
-exec { 'fix-apache':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path    => ['/usr/bin', '/usr/sbin', '/bin']
+# fixes Apache 500 error by fixing typo in wordpress
+exec { 'fix typo':
+  onlyif  => 'test -e /var/www/html/wp-settings.php',
+  command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
